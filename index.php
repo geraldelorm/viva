e55a3e21281bc5fb1c34867758b36a21898ba5e0
geraldelorm <geraldelorm14@gmail.com>
2021-02-11 14:47:16 +0000

    footer minor changes

--- index.php
+++ index.php
@@ -170,8 +170,8 @@
 
   <!-- FOOTER -->
   <footer class="container">
-    <p class="float-right"><a href="#">Back to top</a></p>
-    <p>&copy; 2020 Gerald E. Gbagbe, (Group 5) Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+    <p class="float-right"><a href="#">Back to the top</a></p>
+    <p>&copy; 2020 Gerald E. Gbagbe, (Group 5) ATU. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
   </footer>
 </body>
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
